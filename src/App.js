da5b88d0626cf974f0d23367958d844efcfbea88
Laurin Quast <laurinquast@googlemail.com>
2022-08-08 11:19:35 +0200

    tools reproduction

---
+++ src/App.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+require("./styles.css");
+var merge_1 = require("@graphql-tools/merge");
+function App() {
+}
+exports["default"] = App;
+function test() {
+    var s = merge_1.mergeTypeDefs;
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { mergeTypeDefs } from "@graphql-tools/merge";
+
+export default function App() {
+
+}
+
+function test() {
+  const s = mergeTypeDefs;
+}
